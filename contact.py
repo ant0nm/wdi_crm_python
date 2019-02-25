733415ffbe16bbb638e77ddb6cce769654be0c08
Anton <anton.mois33v@gmail.com>
2019-02-25 10:06:13 -0500

    Implemented and tested str and create methods in the Contact class

--- contact.py
+++ contact.py
@@ -1,20 +1,31 @@
 class Contact:
 
+    # class variables
+    contacts = []
+    next_id = 1
+
     def __init__(self, first_name, last_name, email, note):
         """This method should initialize the contact's attributes"""
+        # contact's basic info
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.note = note
+        # assign each contact an id
+        self.id =  Contact.next_id
+        Contact.next_id += 1
 
     def __str__(self):
-        return "Name: {}\tEmail: {}\tNote: {}".format((self.first_name + ' ' + self.last_name), self.email, self.note)
+        return "Name: {} \t Email: {} \t Note: {}".format((self.first_name + ' ' + self.last_name), self.email, self.note)
 
     @classmethod
-    def create(cls):
+    def create(cls, first_name, last_name, email, note):
         """This method should call the initializer,
         store the newly created contact, and then return it
         """
+        new_contact = Contact(first_name, last_name, email, note)
+        cls.contacts.append(new_contact)
+        return new_contact
 
     @classmethod
     def all(cls):
@@ -59,5 +70,11 @@ class Contact:
 
 # Feel free to add other methods here, if you need them.
 
-anton = Contact('Anton', 'Moiseev', 'athidif94@gmail.com', 'Loves cats')
-print(anton)
+contact1 = Contact.create('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+contact2 = Contact.create('Bit', 'Bot', 'bitbot@gmail.com', 'beep boop')
+print(contact1)
+print(contact2)
+
+print(len(Contact.contacts))
+print(contact1.id)
+print(contact2.id)
