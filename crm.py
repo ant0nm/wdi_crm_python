1da94002d0a2118370c5ebfb49d5db1bc2b5813c
Anton <anton.mois33v@gmail.com>
2019-02-26 13:08:48 -0500

    Enabled creation of new contacts in our db

--- crm.py
+++ crm.py
@@ -48,7 +48,12 @@ class CRM:
         print('Enter a Note: ', end="")
         note = input()
 
-        Contact.create(first_name, last_name, email, note)
+        contact = Contact.create(
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            note = note
+        )
 
 
     def modify_existing_contact(self):
